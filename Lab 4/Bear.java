96a1f8a3c385326a14cd689c844a3a05ed2b93de
221B321 <221B321@juet.co.in>
2025-03-24 15:30:00 +0530

    lab 5

---
+++ Lab 4/Bear.java
@@ -0,0 +1,20 @@
+
+
+public class Bear extends Beverage {
+    @Override
+    protected void AddIngredient() {
+    }
+    // private void PoureIntoCup(){
+    //     System.out.println("Pouring into cup");
+    // }
+    // private void AddBear(){
+    //     System.out.println("Adding bear");
+    // }
+    // public void MakeDrink(){
+    //     System.out.println("Making bear");
+    //     System.out.println("Take Glass");
+    //     PoureIntoCup();
+    //     AddBear();
+    //     System.out.println("Drink is ready");
+    // }
+}
